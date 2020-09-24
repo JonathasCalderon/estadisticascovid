29f86a7f2ad5746756566437ff9010eba0f60cab
Jonathas Calderon <jonathas.calderon@panbox.pe>
2020-09-24 18:14:51 -0400

    navbar funcionando

---
+++ js/navBar.js
@@ -0,0 +1,34 @@
+const navSlide = () => {
+    const burgerIcon = document.querySelector('.icons')
+    const navList = document.querySelector('.nav-links')
+    const navListItems = document.querySelectorAll('.nav-links li')
+
+    burgerIcon.addEventListener('click', () => {
+        navList.classList.toggle('nav-active')
+
+        navListItems.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ''
+            }
+            else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${(index / 7 + 0.3)}s`
+            }
+        })
+    })
+}
+
+const footerDate = () => {
+    const footer = document.querySelector('.footer h6')
+
+    const today = new Date();
+
+    const date = ' ' + today.getFullYear() + '/' + (today.getMonth() + 1) + '/' + today.getDate();
+
+    console.log(date)
+
+    footer.innerHTML = footer.innerHTML + date
+}
+
+
+navSlide()
+footerDate()
